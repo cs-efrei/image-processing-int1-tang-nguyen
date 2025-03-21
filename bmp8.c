44aed2ecf5d78a948fc205608e27a3c3be146271
minousama <minh-lam.nguyen@efrei.net>
2025-03-21 21:03:16 +0100

    little update ;)

--- bmp8.c
+++ bmp8.c
@@ -1,13 +1,22 @@
-//
-// Created by minh- on 21/03/2025.
-//
 
 /* here is the function file*/
 #include "bmp8.h"
+#include <stdlib.h>
+#include <stdio.h>
 /*function that loads the image*/
 t_bmp8 * bmp8_loadImage(const char * filename)
 {
-
+    FILE *file = fopen(filename, "rb");
+    t_bmp8 *fileloc = (t_bmp8*)malloc(sizeof(t_bmp8));
+    if (fileloc == NULL)
+    {
+        printf("Error opening file\n");
+        return NULL;
+    }
+    fread(fileloc, sizeof(t_bmp8), 1, file);
+    fileloc->dataSize = *(unsigned int*)&file[18];
+    fclose(file);
+    return fileloc;
 }
 /*function that saves the image*/
 void bmp8_saveImage(const char *filename, t_bmp8 * img)
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+#include "bmp8.h"
+#include "t_bmp8.h"
 int main(void)
 {
-    printf("Hello, World!\n");
+    bmp8_loadImage("lena_gray.bmp");
+
     return 0;
 }
