0eddbae3a9ebf9c184f201e3cfe062343ee94962
Stéphanie <stephanie.tang@efrei.net>
2025-04-27 19:04:18 +0200

    Update bmp24.h

--- bmp24.h
+++ bmp24.h
@@ -5,6 +5,25 @@
 #include <stdint.h>
 #include "t_bmp24.h"
 
+// Offsets for the BMP header
+#define BITMAP_MAGIC 0x00 // offset 0
+#define BITMAP_SIZE 0x02 // offset 2
+#define BITMAP_OFFSET 0x0A // offset 10
+#define BITMAP_WIDTH 0x12 // offset 18
+#define BITMAP_HEIGHT 0x16 // offset 22
+#define BITMAP_DEPTH 0x1C // offset 28
+#define BITMAP_SIZE_RAW 0x22 // offset 34
+
+// Magical number for BMP files
+#define BMP_TYPE 0x4D42 // 'BM' in hexadecimal
+
+// Header sizes
+#define HEADER_SIZE 0x0E // 14 octets
+#define INFO_SIZE 0x28 // 40 octets
+
+// Constant for the color depth
+#define DEFAULT_DEPTH 0x18 // 24
+
 t_pixel ** bmp24_allocateDataPixels(int width, int height);
 void bmp24_freeDataPixels(t_pixel ** pixels, int height);
 
